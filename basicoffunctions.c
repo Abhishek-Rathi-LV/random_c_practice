893040a0c3a17972d202d329a55b9d451749802c
Abhishek-Rathi-LV <abhishekrathilv@gmail.com>
2026-09-04 13:26:38 +0530

    Add files via upload

---
+++ basicoffunctions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void hello(){
+    printf("Hello World\n");
+    printf("Hello Lunar\n");   
+     printf("Lunar Verse\n");
+
+return;
+}
+int main() {
+    hello();
+    hello();
+    hello();
+    hello();
+    return 0;
+}
+    
